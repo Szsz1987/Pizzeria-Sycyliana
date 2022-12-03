ca33ffe23250e18e1dca96c2556ab4c00a2555cb
Szymon Szuliński <sszulinski@gmail.com>
2022-12-03 23:17:40 +0100

    Add new method initOrderForm which is responsible for adding event listeners to the form, its controls and to the cart button

--- src/js/script.js
+++ src/js/script.js
@@ -60,6 +60,8 @@
       thisProduct.renderInMenu();
       thisProduct.getElements();
       thisProduct.initAccordion();
+      thisProduct.initOrderForm();
+      thisProduct.processOrder();
       console.log('new Product:', thisProduct);
     }
     renderInMenu(){
@@ -89,6 +91,29 @@
         }
         thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
       });
+    }
+    
+    initOrderForm(){
+      const thisProduct = this;
+      console.log(this.initOrderForm);
+      thisProduct.form.addEventListener('submit', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+      for(let input of thisProduct.formInputs){
+        input.addEventListener('change', function(){
+          thisProduct.processOrder();
+        });
+      }
+      thisProduct.cartButton.addEventListener('click', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+    }
+
+    processOrder(){
+      const thisProduct = this;
+      console.log(this.processOrder);
     }
   }
 
