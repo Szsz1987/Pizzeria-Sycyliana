81ac7de1033e70af5e8789f93025d31b748fb702
Szymon Szuliński <sszulinski@gmail.com>
2023-04-13 21:35:05 +0200

    Fix bug in the HourPicker file

--- src/js/components/HourPicker.js
+++ src/js/components/HourPicker.js
@@ -1,4 +1,4 @@
-import BaseWidget from '../components/BaseWidget.js';
+import BaseWidget from '../Components/BaseWidget.js';
 import {select, settings} from '../settings.js';
 import utils from '../utils.js';
 
@@ -6,6 +6,7 @@ class HourPicker extends BaseWidget{
   constructor(wrapper){
     super(wrapper, settings.hours.open);
     const thisWidget = this;
+
     thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.hourPicker.input);
     thisWidget.dom.output = thisWidget.dom.wrapper.querySelector(select.widgets.hourPicker.output);
     thisWidget.initPlugin();
@@ -14,7 +15,7 @@ class HourPicker extends BaseWidget{
 
   initPlugin(){
     const thisWidget = this;
-    // eslint-disable-next-line no-under
+    // eslint-disable-next-line no-undef
     rangeSlider.create(thisWidget.dom.input);
     thisWidget.dom.input.addEventListener('input', function(){
       thisWidget.value = thisWidget.dom.input.value;
@@ -31,6 +32,7 @@ class HourPicker extends BaseWidget{
 
   renderValue(){
     const thisWidget = this;
+
     thisWidget.dom.output.innerHTML = thisWidget.value;
   }
 }
